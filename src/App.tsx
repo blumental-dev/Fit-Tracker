65140e2b4605809bd6b6da3982dca45e1bf632b1
Asaf <abblumental22@gmail.com>
2022-12-19 00:10:04 +0200

    providing theme accordint to preffered device mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { useState } from "react";
 import "./App.css";
 import HomePage from "./layouts/homePage";
 import useGlobalTheme from "./theme/global";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 function App() {
   const [theme] = useGlobalTheme();
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="App">
-        <HomePage />
-      </div>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <HomePage />
+        </div>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
--- src/theme/global.jsx
+++ src/theme/global.jsx
@@ -2,20 +2,50 @@ import React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { amber, deepOrange, grey } from "@mui/material/colors";
+
+const getDesignTokens = (mode) => {
+  console.log({ mode });
+  return {
+    palette: {
+      mode,
+      ...(mode === "light"
+        ? {
+            // palette values for light mode
+            primary: { main: "#007FFF" },
+            divider: amber[200],
+            text: {
+              primary: grey[900],
+              secondary: grey[800],
+            },
+          }
+        : {
+            // palette values for dark mode
+            primary: { main: "#007FFF" },
+            divider: deepOrange[700],
+            background: {
+              default: deepOrange[900],
+              paper: deepOrange[900],
+            },
+            text: {
+              primary: "#e2e2e2",
+              secondary: "#1E1E1E",
+            },
+          }),
+    },
+  };
+};
 
 const useGlobalTheme = () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  console.log({ prefersDarkMode });
 
   const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-        },
-      }),
+    () => createTheme(getDesignTokens(prefersDarkMode ? "dark" : "light")),
     [prefersDarkMode]
   );
 
+  console.log({ theme });
   return [theme];
 };
 
