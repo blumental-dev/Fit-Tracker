33decdf0962b48d1ceecc7089badb25f30f1a362
Asaf <abblumental22@gmail.com>
2022-12-20 20:46:07 +0200

    implementing drawer element

--- src/layouts/homePage/index.tsx
+++ src/layouts/homePage/index.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+import Drawer from "../../components/drawer";
 import Header from "../../containers/header";
 import Topics from "../../containers/topics";
-
 const HomePage: React.FunctionComponent<any> = () => {
   return (
     <>
+      <Drawer />
       <Header />
       <Topics />
     </>
