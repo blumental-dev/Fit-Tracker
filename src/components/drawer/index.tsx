340a77a5ed4ebbe9369c82810f2c546bdb32d8a5
Asaf <abblumental22@gmail.com>
2022-12-20 20:43:50 +0200

    created custom drawer using mui

---
+++ src/components/drawer/index.tsx
@@ -0,0 +1,89 @@
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import * as React from "react";
+import { useDrawer } from "../../context/drawerContext";
+
+const CustomDrawer: React.FunctionComponent<any> = () => {
+  const { state, dispatch } = useDrawer();
+  const toggleDrawer =
+    (isOpen: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      isOpen ? dispatch({ type: "open" }) : dispatch({ type: "close" });
+    };
+
+  const handleDrawerClick = (event: React.BaseSyntheticEvent) => {
+    if (event.target.classList.contains("MuiDrawer-root")) {
+      dispatch({ type: "close" });
+    }
+    return;
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <Drawer
+        anchor="left"
+        open={state.isOpen}
+        PaperProps={{
+          sx: { backgroundColor: "drawerBackground.main" },
+        }}
+        onClick={(event) => handleDrawerClick(event)}
+        onClose={toggleDrawer(false)}
+        hideBackdrop
+      >
+        {list()}
+      </Drawer>
+    </>
+  );
+};
+
+export default CustomDrawer;
