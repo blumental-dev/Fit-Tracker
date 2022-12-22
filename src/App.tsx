1e210601262b39d41820f163f76934559822359b
Asaf <abblumental22@gmail.com>
2022-12-23 00:15:58 +0200

    showing blured background on desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import { Backdrop } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 import { DrawerProvider } from "./context/drawerContext";
 import HomePage from "./layouts/homePage";
 import useGlobalTheme from "./theme/global";
+import { useDeviceQuery } from "./util/breakpoints";
 
 function App() {
   const [theme] = useGlobalTheme();
+  const { isMobile } = useDeviceQuery();
 
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <DrawerProvider>
           <CssBaseline />
-          <div className="App">
-            <HomePage />
-          </div>
+          {!isMobile && (
+            <Backdrop
+              sx={{
+                backgroundColor: "backdrop.main",
+                filter: "blur(10px)",
+              }}
+              open={true}
+            />
+          )}
+          <HomePage />
         </DrawerProvider>
       </ThemeProvider>
     </StyledEngineProvider>
