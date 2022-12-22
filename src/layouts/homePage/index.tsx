8d276b5b6c26e1e99a321c9d2f6e7d38d7256e65
Asaf <abblumental22@gmail.com>
2022-12-23 00:14:40 +0200

    added margin to the dashboard layout on desktop view

--- src/layouts/homePage/index.tsx
+++ src/layouts/homePage/index.tsx
@@ -1,14 +1,28 @@
+import { Box } from "@mui/material";
 import React from "react";
 import Drawer from "../../components/drawer";
 import Header from "../../containers/header";
 import Topics from "../../containers/topics";
+import "./index.css";
 const HomePage: React.FunctionComponent<any> = () => {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        margin: {
+          xs: "none",
+          sm: "none",
+          md: "0.5rem",
+          lg: "0.5rem",
+          xl: "0.5rem",
+        },
+      }}
+    >
       <Drawer />
       <Header />
       <Topics />
-    </>
+    </Box>
   );
 };
 
