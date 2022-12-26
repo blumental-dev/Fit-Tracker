68709400494f0054ce8568eb4663cf98df824984
Asaf <abblumental22@gmail.com>
2022-12-26 15:11:17 +0200

    continued commit

--- src/components/menu/menuItem/index.tsx
+++ src/components/menu/menuItem/index.tsx
@@ -6,31 +6,47 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 type MenuItemProps = {
   label: string;
+  path: string;
   index: number;
   selected: boolean;
+  onSelect: (index: number) => void;
 };
-
 const MenuItem: React.FunctionComponent<MenuItemProps> = ({
   label,
+  path,
   index,
   selected,
-}) => (
-  <ListItem key={`${label}-${index}`} disablePadding>
-    <ListItemButton
-      sx={{
-        backgroundColor: selected ? "backdrop.main" : "none",
-        borderRadius: "1rem",
-        margin: "1rem 0 1rem 0",
-      }}
-    >
-      <ListItemIcon sx={{ color: "primary.dark" }}>
-        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-      </ListItemIcon>
-      <ListItemText primary={label} />
-    </ListItemButton>
-  </ListItem>
-);
+  onSelect,
+}) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <ListItem
+        onClick={() => {
+          onSelect(index);
+          navigate(path);
+        }}
+        key={`${label}-${index}`}
+        disablePadding
+      >
+        <ListItemButton
+          sx={{
+            backgroundColor: selected ? "backdrop.main" : "none",
+            borderRadius: "1rem",
+            margin: "1rem 0 1rem 0",
+          }}
+        >
+          <ListItemIcon sx={{ color: "primary.dark" }}>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={label} />
+        </ListItemButton>
+      </ListItem>
+    </>
+  );
+};
 export default MenuItem;
