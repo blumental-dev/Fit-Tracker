ae1f5016d050dec784161f560f01507a3157f7a4
Asaf <abblumental22@gmail.com>
2022-12-26 15:10:59 +0200

    created and implemented layout using the react router dom

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,17 +1,44 @@
 import { Divider, List } from "@mui/material";
 import Box from "@mui/material/Box";
 
-import React from "react";
+import React, { useState } from "react";
 import MenuItem from "./menuItem";
 type MenuProps = {
   drawerWidth: number | undefined;
   toggleDrawer: (val: boolean) => void;
 };
+type MenuListProps = {
+  id: number;
+  label: string;
+  path: string;
+  selected: boolean;
+};
 const Menu: React.FunctionComponent<MenuProps> = ({
   drawerWidth,
   toggleDrawer,
-}) => (
-  <>
+}) => {
+  const [menuList, setMenuList] = useState<MenuListProps[]>([
+    { id: 1, label: "Dashboard", path: "/", selected: true },
+    { id: 2, label: "Weight Tracker", path: "/weight", selected: false },
+    { id: 3, label: "Objectives", path: "/weight", selected: false },
+    { id: 4, label: "Settings", path: "/weight", selected: false },
+  ]);
+
+  const changeSelectedMenuItem = (index: number) =>
+    setMenuList((prevState) => {
+      for (const obj of prevState) {
+        if (obj.id === index) {
+          if (!obj.selected) {
+            obj.selected = true;
+          }
+        } else {
+          obj.selected = false;
+        }
+      }
+      return prevState;
+    });
+
+  return (
     <Box
       sx={{
         width: drawerWidth,
@@ -23,23 +50,18 @@ const Menu: React.FunctionComponent<MenuProps> = ({
     >
       <Divider />
       <List>
-        {[
-          { label: "Workout Planner", selected: true },
-          { label: "Weight Tracker", selected: false },
-          { label: "Objectives", selected: false },
-          { label: "Settings", selected: false },
-        ].map(({ label, selected }, index) => (
+        {menuList.map((item, index) => (
           <MenuItem
-            key={`${label}-${index}`}
-            label={label}
-            index={index}
-            selected={selected}
+            key={`${item.label}-${index}`}
+            index={item.id}
+            {...item}
+            onSelect={changeSelectedMenuItem}
           />
         ))}
       </List>
       <Divider />
     </Box>
-  </>
-);
+  );
+};
 
 export default Menu;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Topics from "./containers/topics";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <>Error</>,
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: <Topics />,
+      },
+      {
+        path: "weight/",
+        element: <>ADASDADSD</>,
+      },
+      {
+        path: "weight/",
+        element: <>ADASDADSD</>,
+      },
+      {
+        path: "settings/",
+        element: <>Settings Page</>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
